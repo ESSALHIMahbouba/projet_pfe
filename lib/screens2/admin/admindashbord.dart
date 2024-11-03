99dd818e58db4876520aaaaf6a185535951b0468
ESSALHIMahbouba <135343352+ESSALHIMahbouba@users.noreply.github.com>
2024-11-03 14:45:01 +0100

    DREHATT_App

--- lib/screens2/admin/admindashbord.dart
+++ lib/screens2/admin/admindashbord.dart
@@ -181,7 +181,7 @@ class _AdminDashboardState extends State<AdminDashboard> {
                       buildDashboardItem(
                         context,
                         'Données générales',
-                        'lib/assets/icons/me/isens_thumb-removebg-preview.png',
+                        '/lib/assets/icons/me/donnees.png',
                         () {
                           Navigator.push(
                             context,
